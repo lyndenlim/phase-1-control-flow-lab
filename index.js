bfb4b37c9aeca123d0d860ee551104a8c9744922
Lynden Lim <lyndenlim15@gmail.com>
2022-01-09 01:22:24 -0500

    commit tree 96f74d583ba58af98111abad33c62f8a524d3e2c

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400){
+    return "This one is on me!";
+  } else if (ride > 2000 && ride <= 2500){
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+};
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city == "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  switch (tipAmount) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    case "thanks for everything":
+      return "Bye.";
+  }
+};
